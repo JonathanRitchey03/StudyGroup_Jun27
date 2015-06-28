5a0e76941776c1240744bae577124ec7abb8ab9b
Jonathan Ritchey <jonathan@kurbo.com>
2015-06-27 19:20:33 -0700

    Finish easy section

--- Easy/Java/src/com/company/List.java
+++ Easy/Java/src/com/company/List.java
@@ -1,11 +1,14 @@
 package com.company;
 
+import java.util.Random;
+import java.util.Stack;
+
 public class List {
     ListNode head, tail;
-    public List(String data) {
-        head = tail = new ListNode(data);
-    }
-    public void append(String data) {
+    static Random random = new Random();
+    List() {}
+    List(String data) { head=tail=new ListNode(data); }
+    void append(String data) {
         ListNode node = new ListNode(data);
         if ( head == null ) {
             head = tail = node;
@@ -13,11 +16,41 @@ public class List {
             tail = tail.next = node;
         }
     }
-    public void print() {
+    void print() {
         for ( ListNode n = head; n != null; n = n.next ) {
             boolean isThereAnotherNode = n.next != null;
             String rightArrow = "→", carriageReturn = "\n";
             System.out.print(n.data + (isThereAnotherNode ? rightArrow : carriageReturn) );
         }
+    }
+    // Section Easy, Challenge B
+    static List createRandomList(int numNodes, char minLetter, char maxLetter) {
+        List list = new List();
+        for ( int i = 0; i < numNodes; i++ ) {
+            list.append(getRandomLetterString(minLetter,maxLetter));
+        }
+        return list;
+    }
+    static List createPalindromeList(int numNodes, char minLetter, char maxLetter) {
+        List list = new List();
+        Stack<String> stack = new Stack<String>();
+        int midPointIndex = (numNodes-1)/2;
+        boolean isNumNodesOdd = numNodes % 2 == 1;
+        for ( int i = 0; i <= midPointIndex; i++ ) {
+            String randomLetterString = getRandomLetterString(minLetter, maxLetter);
+            boolean skipPushOnExactMidpoint = isNumNodesOdd && i == midPointIndex;
+            if ( !skipPushOnExactMidpoint ) stack.push(randomLetterString);
+            list.append(randomLetterString);
+        }
+        while ( !stack.empty() ) {
+            list.append(stack.pop());
+        }
+        return list;
+    }
+    static String getRandomLetterString(char minLetter, char maxLetter) {
+        int delta = maxLetter - minLetter;
+        int randomDelta = ( delta > 0) ? random.nextInt(delta) : 0;
+        int randomLetterCode = minLetter + randomDelta;
+        return ""+(char)randomLetterCode;
     }
 }
